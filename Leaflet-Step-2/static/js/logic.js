b619ece783de102daebd3fc6e0398d3177e877f8
tiffanydr <72806507+tiffanydr@users.noreply.github.com>
2021-04-18 17:14:23 -0400

    update

--- Leaflet-Step-2/static/js/logic.js
+++ Leaflet-Step-2/static/js/logic.js
@@ -1,205 +1,156 @@
-var myMap=L.map("mapid",{
-    center: [31.51073, -96.4247],
-    zoom: 5
-    });
-    L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
-      attribution: "© <a href='a href='https://www.openstreetmap.org/'>OpenStreetMap</a> contributors, <a href='https://creativecommons.org/licenses/by-sa/2.0/'>CC-BY-SA</a>, Imagery © <a href='https://www.mapbox.com/'>Mapbox</a>",
-      tileSize: 512,
-      maxZoom: 18,
-      zoomOffset: -1,
-      id: "mapbox/streets-v11",
-      accessToken: API_KEY
-    }).addTo(myMap);
+var graymap = L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
+  attribution: "Map data &copy; <a href='https://www.openstreetmap.org/'>OpenStreetMap</a> contributors, <a href='https://creativecommons.org/licenses/by-sa/2.0/'>CC-BY-SA</a>, Imagery © <a href='https://www.mapbox.com/'>Mapbox</a>",
+  maxZoom: 18,
+  id: "mapbox.light",
+  accessToken: API_KEY
+});
 
-    var baseMaps = {
-      "Satellite": satellitemap,
-    }
-    
-    
-    var url="https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_day.geojson"
-    
-    d3.json(url, function(response){
-      console.log(response);
-      function styleInfo(feature){
-        return {
-          opacity: 1,
-          fillOpacity: 1,
-          fillColor: colorIntensity(feature.geometry.coordinates[2]),
-          color: "#000000",
-          radius: feature.properties.mag*5,
-          stroke: true,
-          weight: 0.5
-        }
-      }
-      function colorIntensity(coordinates) {
-    
-        if (coordinates >= 90){
-          return "#ff0000"; 
-        }
-        else if (90> coordinates & coordinates>=70){
-          return "#ff7f50"; 
-        }
-        else if (70> coordinates & coordinates>=50){
-          return "#ff8000"; 
-        }
-        else if (50> coordinates & coordinates>=30){
-          return "#ffbf00"; 
-        }
-        else if (30> coordinates & coordinates>=10){
-          return "#ffff00"; 
-        }
-        else if (10> coordinates){
-          return "#bfff00"; 
-        }
-        
-        }
-      
-      
-      
-          L.geoJson(response, {
-            
-            pointToLayer: function(feature, latlng) {
-              return L.circleMarker(latlng);
-            },
-           
-            style: styleInfo,
-            onEachFeature: function(feature, layer) {
-              layer.bindPopup("Magnitude: " + feature.properties.mag +"<br>Depth:"+ feature.geometry.coordinates[2]+"<br>Location: " + feature.properties.place);
-            }
-          }).addTo(myMap);
-    
-          function getColor(d){
-            return d >90 ? "#ff0000":
-                   d >70 ?  "#ff7f50":
-                   d >50 ? "#ff8000":
-                   d >30 ? "#ffbf00":
-                   d >10 ? "#ffff00":
-                            "#bfff00";
-                  
-          }
-    
-    
-          var legend= L.control({position: "bottomright"});
-          legend.onAdd=function(myMap){
-            var div = L.DomUtil.create("div", "info legend"),
-            grades =[-10,10,30,50,70,90];
-            for (var i = 0; i < grades.length; i++) {
-              div.innerHTML +=
-                  '<i style="background:' + getColor(grades[i] + 1) + '"></i> ' +
-                  grades[i] + (grades[i + 1] ? '&ndash;' + grades[i + 1] + '<br>' : '+');
-          }
-      
-          return div;
-      };
-      legend.addTo(myMap);
+var satellitemap = L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
+  attribution: "Map data &copy; <a href='https://www.openstreetmap.org/'>OpenStreetMap</a> contributors, <a href='https://creativecommons.org/licenses/by-sa/2.0/'>CC-BY-SA</a>, Imagery © <a href='https://www.mapbox.com/'>Mapbox</a>",
+  maxZoom: 18,
+  id: "mapbox.streets-satellite",
+  accessToken:API_KEY
+});
+
+var outdoors = L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
+  attribution: "Map data &copy; <a href='https://www.openstreetmap.org/'>OpenStreetMap</a> contributors, <a href='https://creativecommons.org/licenses/by-sa/2.0/'>CC-BY-SA</a>, Imagery © <a href='https://www.mapbox.com/'>Mapbox</a>",
+  maxZoom: 18,
+  id: "mapbox.outdoors",
+  accessToken: API_KEY
+});
+
+
+var map = L.map("mapid", {
+  center: [
+    40.7, -94.5
+  ],
+  zoom: 3,
+  layers: [graymap, satellitemap, outdoors]
+});
+
+
+graymap.addTo(map);
+
+var tectonicplates = new L.LayerGroup();
+var earthquakes = new L.LayerGroup();
+
+var baseMaps = {
+ "Satellite": satellitemap,
+  "Grayscale": graymap,
+  "Outdoors": outdoors
+};
+
+
+var overlays = {
+  "Tectonic Plates": tectonicplates,
+  Earthquakes: earthquakes
+};
+
+
+L
+  .control
+  .layers(baseMaps, overlays)
+  .addTo(map);
+
+
+d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson", function(data) {
+
+  
+  function styleInfo(feature) {
+    return {
+      opacity: 1,
+      fillOpacity: 1,
+      fillColor: getColor(feature.properties.mag),
+      color: "#000000",
+      radius: getRadius(feature.properties.mag),
+      stroke: true,
+      weight: 0.5
     };
-    
+  }
 
-  var tectonicplates = new L.LayerGroup();
-  var earthquakes = new L.LayerGroup();
-}
-  var baseMaps = {
-   "Satellite": satellitemap,
-  
-  };
-  
-  var overlays = {
-    "Tectonic Plates": tectonicplates,
-    Earthquakes: earthquakes
-  };
-
-  L
-    .control
-    .layers(baseMaps, overlays)
-    .addTo(map);
-  
-
-    var url="https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_day.geojson"
-   
-    d3.json(url, function(response){
-      console.log(response);
-    function styleInfo(feature) {
-      return {
-        opacity: 1,
-        fillOpacity: 1,
-        fillColor: getColor(feature.properties.mag),
-        color: "#000000",
-        radius: getRadius(feature.properties.mag),
-        stroke: true,
-        weight: 0.5
-      };
+ 
+  function getColor(magnitude) {
+    switch (true) {
+    case magnitude > 5:
+      return "#ea2c2c";
+    case magnitude > 4:
+      return "#ea822c";
+    case magnitude > 3:
+      return "#ee9c00";
+    case magnitude > 2:
+      return "#eecc00";
+    case magnitude > 1:
+      return "#d4ee00";
+    default:
+      return "#98ee00";
     }
+  }
 
+ 
   function getRadius(magnitude) {
-      if (magnitude === 0) {
-        return 1;
-      }
-  
-      return magnitude * 4;
+    if (magnitude === 0) {
+      return 1;
     }
+
+    return magnitude * 4;
+  }
+
+
+  L.geoJson(data, {
   
+    pointToLayer: function(feature, latlng) {
+      return L.circleMarker(latlng);
+    },
 
-    L.geoJson(response, {
+    style: styleInfo,
 
-      pointToLayer: function(feature, latlng) {
-        return L.circleMarker(latlng);
-      },
-     
-      style: styleInfo,
+    onEachFeature: function(feature, layer) {
+      layer.bindPopup("Magnitude: " + feature.properties.mag + "<br>Location: " + feature.properties.place);
+    }
     
-      onEachFeature: function(feature, layer) {
-        layer.bindPopup("Magnitude: " + feature.properties.mag + "<br>Location: " + feature.properties.place);
-      }
-    
-    }).addTo(earthquakes);
-  
-    earthquakes.addTo(map);
-  
+  }).addTo(earthquakes);
 
-    var legend = L.control({
-      position: "bottomright"
+  earthquakes.addTo(map);
+
+  var legend = L.control({
+    position: "bottomright"
+  });
+
+  
+  legend.onAdd = function() {
+    var div = L
+      .DomUtil
+      .create("div", "info legend");
+
+    var grades = [0, 1, 2, 3, 4, 5];
+    var colors = [
+      "#98ee00",
+      "#d4ee00",
+      "#eecc00",
+      "#ee9c00",
+      "#ea822c",
+      "#ea2c2c"
+    ];
+
+    for (var i = 0; i < grades.length; i++) {
+      div.innerHTML += "<i style='background: " + colors[i] + "'></i> " +
+        grades[i] + (grades[i + 1] ? "&ndash;" + grades[i + 1] + "<br>" : "+");
+    }
+    return div;
+  };
+
+  legend.addTo(map);
+
+  d3.json("https://raw.githubusercontent.com/fraxen/tectonicplates/master/GeoJSON/PB2002_boundaries.json",
+    function(platedata) {
+
+      L.geoJson(platedata, {
+        color: "orange",
+        weight: 2
+      })
+      .addTo(tectonicplates);
+
+
+      tectonicplates.addTo(map);
     });
-  
-  
-    legend.onAdd = function() {
-      var div = L
-        .DomUtil
-        .create("div", "info legend");
-  
-      var grades = [0, 1, 2, 3, 4, 5];
-      var colors = [
-        "#98ee00",
-        "#d4ee00",
-        "#eecc00",
-        "#ee9c00",
-        "#ea822c",
-        "#ea2c2c"
-      ];
-  
-      for (var i = 0; i < grades.length; i++) {
-        div.innerHTML += "<i style='background: " + colors[i] + "'></i> " +
-          grades[i] + (grades[i + 1] ? "&ndash;" + grades[i + 1] + "<br>" : "+");
-      }
-      return div;
-    };
-
-    legend.addTo(map);
-    
-    var url="https://raw.githubusercontent.com/fraxen/tectonicplates/master/GeoJSON/PB2002_boundaries.json"
-     
-    d3.json(url, function(response){
-      console.log(response);
-  
-    function (platedata);
-    {
-           L.geoJson(platedata, {
-            color: "orange",
-            weight: 2
-          });
-        }
-  
-          .addTo(tectonicplates);
-    
-      };
-          tectonicplates.addTo(map);
-        });
+});
